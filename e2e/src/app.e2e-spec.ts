8344a36bda8e83fd5b32de3b4e4ccb4d09d6a9f9
shirasaka <shirasaka@n-create.co.jp>
2020-02-28 18:37:59 +0900

    Angular update 8 to 9

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to db-manager!');
+    expect(page.getTitleText()).toEqual('db-manager app is running!');
   });
 
   afterEach(async () => {
